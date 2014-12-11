c611d3f85c99436074683d7773b313fc1b526856
Arjan van der Velde <vandervelde.ag@gmail.com>
2014-12-11 00:34:18 -0500

    functions

---
+++ python1/Python_I/scripts/functions.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+def mean(numbers): # this defines a function `mean()`
+    mysum = sum(numbers)
+    return float(mysum) / len(numbers)
+
+def hello():
+    print("Hello World")
+    
+def main(): # this defines a function `main()`
+    x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    print(mean(x))
+    print(type(mean(x)))
+    print(type(hello()))
+
+if __name__ == "__main__":
+    main()
